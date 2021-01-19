ca6b51da5261fd82492866ffecb9fe5ac6826b29
Maftuna1707 <72442735+Maftuna1707@users.noreply.github.com>
2021-01-19 19:48:28 +0500

    Changed

--- main.js
+++ main.js
@@ -1,102 +1,40 @@
 let a = true;
-<<<<<<< HEAD
-// document.getElementById("aboutRus").onclick = function(){
-//     a=false;
-//     document.getElementById("russia").style.display = "block";
-//     document.getElementById("aboutUzb").style.background = "white";
-//     document.getElementById("aboutUzb").style.color = "black";
-// }
-function rus(){
-=======
 document.getElementById("aboutRus").onclick = function(){
->>>>>>> fb01ec66e7dc4ea1b5823f68bf90f1a85733a9d4
     a=false;
     document.getElementById("russia").style.display = "block";
     document.getElementById("aboutUzb").style.background = "white";
     document.getElementById("aboutUzb").style.color = "black";
 }
-<<<<<<< HEAD
-// document.getElementById("aboutUzb").onclick = function(){
-//     a=false;
-//     document.getElementById("uzbekistan").style.display = "block";
-//     document.getElementById("aboutUzb").style.background = "cornflowerblue";
-//     document.getElementById("aboutUzb").style.color = "white";
-//     // document.getElementById("uzbekistan").style.zIndex=1000;
-// }
-function uzb(){
-=======
 document.getElementById("aboutUzb").onclick = function(){
->>>>>>> fb01ec66e7dc4ea1b5823f68bf90f1a85733a9d4
     a=false;
     document.getElementById("uzbekistan").style.display = "block";
     document.getElementById("aboutUzb").style.background = "cornflowerblue";
     document.getElementById("aboutUzb").style.color = "white";
-<<<<<<< HEAD
-}
-// document.getElementById("aboutUK").onclick = function(){
-//     a=false;
-//     document.getElementById("britan").style.display = "block";
-//     document.getElementById("aboutUzb").style.background = "white";
-//     document.getElementById("aboutUzb").style.color = "black";
-//
-//     // alert("salom1");
-// }
-function great_britan(){
-=======
     // document.getElementById("uzbekistan").style.zIndex=1000;
 }
 
 document.getElementById("aboutUK").onclick = function(){
->>>>>>> fb01ec66e7dc4ea1b5823f68bf90f1a85733a9d4
     a=false;
     document.getElementById("britan").style.display = "block";
     document.getElementById("aboutUzb").style.background = "white";
     document.getElementById("aboutUzb").style.color = "black";
-<<<<<<< HEAD
-}
-function america (){
-=======
-    
+
     // alert("salom1");
 }
 document.getElementById("aboutUS").onclick = function(){
->>>>>>> fb01ec66e7dc4ea1b5823f68bf90f1a85733a9d4
     a=false;
     document.getElementById("america").style.display = "block";
     document.getElementById("aboutUzb").style.background = "white";
     document.getElementById("aboutUzb").style.color = "black";
-<<<<<<< HEAD
-}
-// document.getElementById("aboutUS").onclick = function(){
-//     a=false;
-//     document.getElementById("america").style.display = "block";
-//     document.getElementById("aboutUzb").style.background = "white";
-//     document.getElementById("aboutUzb").style.color = "black";
-//     // alert("salom2");
-// }
-// document.getElementById("aboutFr").onclick = function(){
-//     a=false;
-//     document.getElementById("france").style.display = "block";
-//     document.getElementById("aboutUzb").style.background = "white";
-//     document.getElementById("aboutUzb").style.color = "black";
-//     // alert("salom3");
-//     // document.getElementById("france").style.zIndex=1000;
-// }
-function france (){
-=======
     // alert("salom2");
 }
 document.getElementById("aboutFr").onclick = function(){
->>>>>>> fb01ec66e7dc4ea1b5823f68bf90f1a85733a9d4
     a=false;
     document.getElementById("france").style.display = "block";
     document.getElementById("aboutUzb").style.background = "white";
     document.getElementById("aboutUzb").style.color = "black";
-<<<<<<< HEAD
-=======
     // alert("salom3");
     // document.getElementById("france").style.zIndex=1000;
->>>>>>> fb01ec66e7dc4ea1b5823f68bf90f1a85733a9d4
 }
 document.getElementById("aboutUzb").onmouseenter = function() {
     document.getElementById("uzbekistan").style.zIndex=1000;
@@ -110,43 +48,25 @@ document.getElementById("aboutRus").onmouseenter = function() {
 }
 document.getElementById("aboutRus").onmouseleave = function() {
     if(a){
-<<<<<<< HEAD
         document.getElementById("russia").style.display = "none"
     }
-=======
-    document.getElementById("russia").style.display = "none"
-}
->>>>>>> fb01ec66e7dc4ea1b5823f68bf90f1a85733a9d4
 }
 document.getElementById("aboutUK").onmouseenter = function() {
     document.getElementById("britan").style.display= "blocked"
     document.getElementById("britan").style.zIndex=1000;
-<<<<<<< HEAD
 }
 document.getElementById("aboutUK").onmouseleave = function() {
     if(a){
         document.getElementById("britan").style.display= "none"
     }
 }
-=======
-}
-document.getElementById("aboutUK").onmouseleave = function() {
-    if(a){
-    document.getElementById("britan").style.display= "none"
-    }
-}
->>>>>>> fb01ec66e7dc4ea1b5823f68bf90f1a85733a9d4
 document.getElementById("aboutUS").onmouseenter = function() {
     document.getElementById("america").style.display= "blocked"
     document.getElementById("america").style.zIndex=1000;
 }
 document.getElementById("aboutUS").onmouseleave = function() {
     if(a){
-<<<<<<< HEAD
         document.getElementById("america").style.display= "none"
-=======
-    document.getElementById("america").style.display= "none"
->>>>>>> fb01ec66e7dc4ea1b5823f68bf90f1a85733a9d4
     }
 }
 document.getElementById("aboutFr").onmouseenter = function() {
@@ -155,11 +75,7 @@ document.getElementById("aboutFr").onmouseenter = function() {
 }
 document.getElementById("aboutFr").onmouseleave = function() {
     if(a){
-<<<<<<< HEAD
         document.getElementById("france").style.display= "none"
-=======
-    document.getElementById("france").style.display= "none"
->>>>>>> fb01ec66e7dc4ea1b5823f68bf90f1a85733a9d4
     }
 }
 
